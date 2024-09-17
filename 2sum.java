3b4093c3600804cac0610de78ca7add2c20b50d2
Saksham Bansal <152879899+Saksham-Bansal-3110@users.noreply.github.com>
2024-09-18 00:44:46 +0530

    Create 2sum.java

---
+++ 2sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i=0; i<nums.length; i++){
+            int difference = target - nums[i];
+            if (map.containsKey(difference) && map.get(difference)!=i){
+                return new int[]{i,map.get(difference)};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[] {};
+    }
+}
